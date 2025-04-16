757b136c2cf444f7d37a11e3042ba38b709aa26c
Devkumar Patel <95736260+devpatel43543@users.noreply.github.com>
2025-04-16 13:24:47 -0300

    Update Interview.jsx

--- frontend/src/pages/Interview.jsx
+++ frontend/src/pages/Interview.jsx
@@ -27,6 +27,7 @@ const { questions, jdID } = useContext(QuestionContext);
         const minutes = Math.floor(time / 60);
         const seconds = time % 60;
         return `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+
     };
 
     const startRecording = async () => {
